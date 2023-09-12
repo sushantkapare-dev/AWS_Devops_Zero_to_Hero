4d15ca6b318cd38f5251d8c055569e9cfd8d0c0c
Sushant Kapare <sushantkapare1717@gmail.com>
2023-09-12 12:02:32 +0530

    Update main.tf

--- Terraform/Manifest/Ec2_Creation/main.tf
+++ Terraform/Manifest/Ec2_Creation/main.tf
@@ -1,1 +1,22 @@
 
+terraform {
+  backend "s3" {
+    bucket = "mynewbusket1306"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-2"
+
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0c9c942bd7bf113a2"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "MyEc2Instance"
+  }
+}
