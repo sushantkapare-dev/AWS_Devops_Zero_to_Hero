4662eb42371ac027267943fe7df092a03d50b794
sushantkapare1717@gmail.com <sushant@13.com>
2023-12-08 18:44:57 +0530

    added eks project

---
+++ Terraform/Manifest/05_AWS_EKS/main.tf
@@ -0,0 +1,99 @@
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "eks_cluster_policy" {
+  name       = "eks-cluster-policy"
+  roles      = [aws_iam_role.eks_cluster.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Get default VPC id
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get public subnets in VPC
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "EKS_CLOUD"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.public.ids
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name = "eks-node-group-cloud"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.example.name
+}
+
+# Create managed node group
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "managed-nodes"
+  node_role_arn   = aws_iam_role.example.arn
+
+  subnet_ids = data.aws_subnets.public.ids
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+  instance_types = ["t2.micro"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_eks_cluster.eks
+  ]
+}
+
+
+
---
+++ Terraform/Manifest/05_AWS_EKS/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "ap-northeast-2"
+  
+}
