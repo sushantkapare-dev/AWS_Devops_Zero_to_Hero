d8b42e2ef8e93c75af6d119448d81518c641e2d2
sushantkapare1717@gmail.com <sushant@13.com>
2023-12-15 09:26:45 +0530

    added datatypes.go file

---
+++ Go/04_datatypes.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var userName string
+	var age int
+
+	userName = "Sushant"
+	age = 27
+
+	fmt.Println("Helle", userName, "you are", age, "years old")
+}
