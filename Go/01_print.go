2b936948a557f066024d95cbcd26f1b48825a14d
sushantkapare1717@gmail.com <sushant@13.com>
2023-12-15 09:18:19 +0530

    added changes

---
+++ Go/01_print.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name = "Sushant"
+	fmt.Print("Hello World")         // Print does not add a new line at the end
+	fmt.Println("Hello World")       // Println adds a new line at the end
+	fmt.Println("Hello World", name) // , adds a space between the strings
+}
