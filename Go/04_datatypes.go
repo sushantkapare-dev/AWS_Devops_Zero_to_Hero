89d0538001493f47adaf9a796ee926b2f4e40c39
sushantkapare1717@gmail.com <sushant@13.com>
2023-12-15 09:39:06 +0530

    added userinput.go

--- Go/04_datatypes.go
+++ Go/04_datatypes.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 
-	var userName string
+	var userName string // Declering an variable
 	var age int
 
-	userName = "Sushant"
+	userName = "Sushant" //The go compiler will infer the type of the variable
 	age = 27
 
 	fmt.Println("Helle", userName, "you are", age, "years old")
