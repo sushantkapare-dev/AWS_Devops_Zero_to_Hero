c796b53f71b80a4e8d1a125b2066ded3b06d872f
sushantkapare-dev <sushantkapare1717@gmail.com>
2025-01-08 08:54:42 +0530

    Create Environment Setup Script.sh

---
+++ Linux_Shell_scripting/Linux_Scripts/Environment Setup Script.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash
+ # Customize for your specific environment setup
+ echo "Setting up development environment..."
+ # Install necessary packages, configure settings, etc.
+ echo "Development environment set up successfully."
